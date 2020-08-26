7f53ecd46a91dd41460dec4e3e6c097331e6f86a
Cissy <1395422644@qq.com>
2020-08-26 23:37:30 +0800

    cart-1

--- src/components/OrderHeader.vue
+++ src/components/OrderHeader.vue
@@ -1,13 +1,88 @@
 <template>
-  <div>order-header</div>
+  <div class="order-header">
+      <div class="container">
+        <div class="left">
+          <a href="/#/index"></a>
+        </div>
+        <div class="middle">
+          <h2>{{title}}<slot name="tip"></slot></h2>
+        </div>
+        <div class="right">
+          cissy
+        </div>
+      </div>
+    </div>
 </template>
 
 <script>
 export default {
-  name:"order-header"
+  name:"order-header",
+  props:{
+    title:String
+  }
 }
 </script>
 
-<style lang="stylus" scoped>
+<style lang="scss" >
+@import "../assets/scss/base.scss";
+@import "../assets/scss/mixin.scss";
+@import "../assets/scss/config.scss";
+.order-header{
+  .container {
+      height: 112px;
+      line-height: 112px;
+      display: flex;
+      align-items: center;
+      position: relative;
+      .left {
+        display: inline-block;
+        height: 55px;
+        width: 55px;
+        background-color: $colorA;
+        a {
+          display: inline-block;
+          width: 110px;
+          height: 55px;
+          // border:1px solid red;
+          &::before {
+            content: " ";
+            display: inline-block;
+            @include bgImg(55px, 55px, "/imgs/mi-logo.png", 55px);
+            transition: margin 0.2s;
+          }
+          &::after {
+            content: " ";
+            @include bgImg(55px, 55px, "/imgs/mi-home.png", 55px);
+          }
+          &:hover:before {
+            margin-left: -55px;
+            transition: margin 0.2s;
+          }
+        }
+      }
+      .middle {
+        flex: 1;
+        padding-left: 53px;
+        font-weight: bold;
+        >h2{
+        font-size: 28px;
+        color: #333;
+          >span{
+           font-size: 14px;
+            color: #999;
+            margin-left: 17px;
+        }
+        }
+       
+      }
+      .right {
+        width: 50px;
+        text-align: right;
+        font-size: 16px;
+        color: #666;
+        font-weight: bold;
+      }
+    }
+}
 
 </style>
--- src/pages/cart.vue
+++ src/pages/cart.vue
@@ -1,21 +1,92 @@
 
 <template>
-  <div>
-   cart
-    <nav-header></nav-header>
+  <div class="cart">
    
+    <order-header title="我的购物车">
+     <template v-slot:tip>
+       
+         <span>温馨提示：产品是否购买成功，以最终下单为准哦，请尽快结算</span>
+       
+     </template>
+    </order-header>
+    <div class="main">
+        <div class="container">
+         <div class="card">
+         <div class="card-top">
+          <div class="option"><span></span>全选</div>
+          <div class="name">商品名称</div>
+          <div class="price">单价</div>
+          <div class="num">数量</div>
+          <div class="count">小计</div>
+          <div class="operate">操作</div>
+         </div>
+          <div class="card-item">
+          
+          <div class="option"><span></span></div>
+          <div class="name">
+            <img src="" alt="">
+            小米8 6GB 全息幻彩紫 64GB
+          </div>
+          <div class="price">1999元</div>
+          <div class="num">
+            <span>-</span>
+            <span>1</span>
+            <span>+</span>
+          </div>
+          <div class="count">1999元</div>
+          <div class="operate">x</div>
+          </div>
+         </div>
+        <div class="total"></div>
+        </div>
+    </div>
     <nav-footer></nav-footer>
   </div>
 </template>
 
 <script>
-import navHeader from '../components/NavHeader'
+import orderHeader from '../components/OrderHeader'
 import navFooter from '../components/NavFooter'
 export default {
   name:'cart',
   components:{
-    navHeader,
+    orderHeader,
     navFooter
   }
 }
 </script>
+
+<style lang="scss">
+  .cart{
+    
+    >.main{
+      padding-top:30px;
+      background-color: #f5f5f5;
+      >.container{
+        >.card{
+          padding: 0 43px;
+          background: #fff;
+          .card-top,.card-item{
+            display: flex;
+            height: 80px;
+            border-bottom: 1px solid #e5e5e5;
+            text-align: center;
+            align-items: center;
+            // border: 1px solid green;
+            >div{border: 1px solid red;}
+            .option,.price,.count,.operate{width: 11%;}
+            .num{width: 18%;}
+            >.name{flex: 1;}
+          }
+          .card-top{
+            font-size: 14px;
+            color: #666;
+          }
+          .card-item{
+           color: #333;
+          }
+        }
+      }
+    }
+  }
+</style>
